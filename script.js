5934899d6da1b91c273b58e8cc05cd66bf965755
Josh Sandquist <joshdsand@gmail.com>
2023-02-09 11:53:27 -0700

    created function to take password legth input

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var generatePassword = function() {
-  var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var numbers = [0,1,2,3,4,5,6,7,8,9];
-  var special =[ '!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var special =[ '!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+
+var messages = function() {
+  passwordLength = prompt("Please enter the number of characters between 8-128")
+  if (passwordLength < 8 || passwordLength> 128) {
+    alert("Password length must be a number between 8 and 126")
+  }
+
 }
 
-generatePassword()
 
 
 
 
 
-
-
+console.log(messages)
 
 
 // Assignment Code
